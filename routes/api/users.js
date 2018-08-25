3bd57fd02c578610f249d419ab417ce221a3e276
traneric94 <traneric94@gmail.com>
2018-08-24 22:03:43 -0700

    Login Functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jsonwebtoken = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 router.get("/test", (req, res) => res.json({msg: "This is the users route"}));
 
@@ -26,15 +28,48 @@ router.post('/register', (req, res) => {
         })
 
         bcrypt.genSalt(10, (err, salt) => {
-  bcrypt.hash(newUser.password, salt, (err, hash) => {
-    if (err) throw err;
-    newUser.password = hash;
-    newUser.save()
-      .then(user => res.json(user))
-      .catch(err => console.log(err));
-  })
-})
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
       }
+    })
+})
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({email: 'This user does not exist'});
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = {id: user.id, name: user.name};
+
+            jsonwebtoken.sign(
+              payload,
+              keys.secretOrKey,
+              // Tell the key to expire in one hour
+              {expiresIn: 3600},
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
+          } else {
+            return res.status(400).json({password: 'Incorrect password'});
+          }
+        })
     })
 })
 
