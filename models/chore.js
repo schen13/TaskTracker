2bf63f3e7082010b462542b524bd80b9d1ed142e
bjjeong <bjjeong@umich.edu>
2018-08-27 16:14:24 -0700

    Update validation for Chore

--- models/chore.js
+++ models/chore.js
@@ -2,29 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ChoreSchema = new Schema ({
-    name: {
-        type: String,
-        default: 'Chore'
-    },
-    description: {
-        type: Text
-    },
-    userId: {
-        type: Number,
-        required: true
-    },
-    deadline: {
-        type: Date,
-        required: true
-    },
-    estTime: {
-        type: Number,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
+  name: {
+    type: String,
+      default: 'Chore'
+  },
+  description: {
+      type: Text
+  },
+  userId: {
+    type: Number,
+      required: true
+  },
+  deadline: {
+    type: Date,
+      required: true
+  },
+  estTime: {
+    type: Number,
+      required: true
+  },
+  completed: {
+    type: Boolean,
+      default: false
+  }
 });
 
 const Chore = mongoose.model('chores', ChoreSchema);
--- validation/chore.js
+++ validation/chore.js
@@ -4,14 +4,19 @@ const isEmpty = require('./is-empty');
 module.exports = function validateChoreInput(data) {
     let errors = {};
 
-    data.text = !isEmpty(data.text) ? data.text : '';
+    data.name = !isEmpty(data.name) ? data.name : '';
+    data.description = !isEmpty(data.description) ? data.description : '';
+    data.userId = !isEmpty(data.userId) ? data.userId : '';
+    data.deadline = !isEmpty(data.deadline) ? data.deadline : '';
+    data.estTime = !isEmpty(data.estTime) ? data.estTime : '';
+    data.completed = !isEmpty(data.completed) ? data.completed : '';
 
-    if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
-        errors.text = 'Event name must be between 10 and 300 characters';
+    if (!Validator.isLength(data.name, { min: 3, max: 300 })) {
+        errors.name = 'Event name must be between 3 and 300 characters';
     }
 
-    if (Validator.isEmpty(data.text)) {
-        errors.text = 'Text field is required';
+    if (Validator.isEmpty(data.name)) {
+        errors.name = 'Chore name is required';
     }
 
     return {
