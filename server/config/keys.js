21fe37623c13ce505eb9e9520306bf55c5a90053
Steven Chen <steven.chen313@gmail.com>
2018-08-27 17:54:19 -0700

    Reconfigure file structure

--- server/config/keys.js
+++ server/config/keys.js
@@ -3,4 +3,4 @@
 module.exports = {
   mongoURI: 'mongodb://traveluserapp123:3travelpassword3@ds131902.mlab.com:31902/travel_app',
   secretOrKey: 'secret'
-}
+};
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/user');
 const jsonwebtoken = require('jsonwebtoken');
-const keys = require('../../config/keys');
+const keys = require('../config/keys');
 const passport = require('passport');
 const validateRegisterInput = require('../validation/register');
 const validateLoginInput = require('../validation/login');
 
-router.get('/test', (req, res) => res.json({msg: 'This is the users route'}));
+router.get('/test', (req, res) => res.json({ msg: 'This is the users route' }));
 
-router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
   res.json({
     id: req.user.id,
     username: req.user.username,
     email: req.user.email
   });
-})
+});
 
 router.post('/register', (req, res) => {
   console.log(req.body);
@@ -31,7 +31,7 @@ router.post('/register', (req, res) => {
     .then(user => {
       if (user) {
         // Throw a 400 error if the email address already exists
-        return res.status(400).json({email: 'A user has already registered with this address'})
+        return res.status(400).json({ email: 'A user has already registered with this address' })
       } else {
         // Otherwise create a new user
 
@@ -41,7 +41,7 @@ router.post('/register', (req, res) => {
           username: req.body.username,
           email: req.body.email,
           password: req.body.password
-        })
+        });
 
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -58,7 +58,7 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
-  
+
   if (!isValid) {
     return res.status(400).json(errors);
   }
@@ -66,22 +66,22 @@ router.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({email})
+  User.findOne({ email })
     .then(user => {
       if (!user) {
-        return res.status(404).json({email: 'This user does not exist'});
+        return res.status(404).json({ email: 'This user does not exist' });
       }
 
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            const payload = {id: user.id, username: user.username};
+            const payload = { id: user.id, username: user.username };
 
             jsonwebtoken.sign(
               payload,
               keys.secretOrKey,
               // Tell the key to expire in one hour
-              {expiresIn: 3600},
+              { expiresIn: 3600 },
               (err, token) => {
                 res.json({
                   success: true,
@@ -89,7 +89,7 @@ router.post('/login', (req, res) => {
                 });
               });
           } else {
-            return res.status(400).json({password: 'Incorrect password'});
+            return res.status(400).json({ password: 'Incorrect password' });
           }
         })
     })
