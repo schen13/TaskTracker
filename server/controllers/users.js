3338853f45f663b3d0801fa5335a6533463e4217
traneric94 <traneric94@gmail.com>
2018-08-27 17:19:21 -0700

    Fix user routes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../../models/User');
+const User = require('../models/User');
 const jsonwebtoken = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
-const validateRegisterInput = require('../../validation/register');
-const validateLoginInput = require('../../validation/login');
-// const isEmpty = require('../../validation/is-empty');
+const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
 
 router.get('/test', (req, res) => res.json({msg: 'This is the users route'}));
 
