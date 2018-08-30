6ffa312c65cc1941a8c129c2440c336efbf1284d
Steven Chen <steven.chen313@gmail.com>
2018-08-29 18:42:50 -0700

    Make login and signup auth routes

--- frontend/src/components/root.jsx
+++ frontend/src/components/root.jsx
@@ -7,18 +7,20 @@ import {
 import NavBarContainer from './navbar/navbar_container';
 import SignUpFormContainer from "./session_form/signup_form_container";
 import LogInFormContainer from "./session_form/login_form_container";
+import GroupIndexContainer from './group/group_index_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const Root = () => (
   <div>
     <header>
       <Link to="/" className="header-link">
-        <h1>GROUP_APP_NAME</h1>
+        <h1>TaskTracker</h1>
       </Link>
       <NavBarContainer />
     </header>
-    <Route exact path="/login" component={LogInFormContainer} />
-    <Route exact path="/signup" component={SignUpFormContainer} />
-
+    <AuthRoute exact path="/login" component={LogInFormContainer} />
+    <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+    {/* <ProtectedRoute path="/" component={GroupIndexContainer} /> */}
   </div>
 );
 
