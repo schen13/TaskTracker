f7643fb62016df2a7df3d6973a96cd8d013ee257
Steven Chen <steven.chen313@gmail.com>
2018-08-31 09:33:59 -0700

    Fix duplicate user fetch action

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -2,17 +2,17 @@ import * as UserAPIUtil from '../util/user_api_util';
 
 export const RECEIVE_ALL_USERS = 'RECEIVE_ALL_USERS';
 export const RECEIVE_SINGLE_USER = 'RECEIVE_SINGLE_USER';
-export const RECEIVE_USERS = 'RECEIVE_USERS';
+// export const RECEIVE_USERS = 'RECEIVE_USERS';
 
 const receiveSingleUser = payload => ({
   type: RECEIVE_SINGLE_USER,
   payload: payload.data
 });
 
-const receiveUsers = payload => ({
-  type: RECEIVE_USERS,
-  users: payload.data.user
-});
+// const receiveUsers = payload => ({
+//   type: RECEIVE_USERS,
+//   users: payload.data.user
+// });
 
 export const fetchAllUsers = () => dispatch => (
   UserAPIUtil.fetchAllUsers()
@@ -24,9 +24,9 @@ export const fetchSingleUser = id => dispatch => (
     .then(user => dispatch(receiveSingleUser(user)))
 );
 
-export const fetchUsers = () => dispatch => (
-  UserAPIUtil.fetchUsers().then(users => dispatch(receiveUsers(users)))
-);
+// export const fetchUsers = () => dispatch => (
+//   UserAPIUtil.fetchUsers().then(users => dispatch(receiveUsers(users)))
+// );
 const receiveAllUsers = payload => ({
   type: RECEIVE_ALL_USERS,
   payload: payload.data
