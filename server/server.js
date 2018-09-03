627553dcec8547e2ff87bbae2c3999cec9e3f77e
traneric94 <traneric94@gmail.com>
2018-09-02 22:43:25 -0700

    Refactor serving static assets

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const server = require('http').Server(app);
 server.listen(port, () => console.log(`websockets are running on ${port}`));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("../client/src/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "client", "public", "index.html"));
   });
