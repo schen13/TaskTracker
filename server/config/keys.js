a3d6d50a0b1a3945d5faa8ac5d6b4c53718a4265
bjjeong <bjjeong@umich.edu>
2018-09-03 05:07:55 -0700

    Fix merge conflict

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,11 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-<<<<<<< HEAD
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-=======
     module.exports = require('./keys_prod');
 } else {
     module.exports = require('./keys_dev');
->>>>>>> 69d27f0fe31ef49a33e7d290e42817fac27f79de
 }
