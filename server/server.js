28fb2ccdfd3fe3c66c77324b5fc48bb13cde86de
Steven Chen <steven.chen313@gmail.com>
2018-09-04 17:03:09 -0700

    Fix server bug

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ server.listen(port, () => console.log(`websockets are running on ${port}`));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/build"));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
   });
 }
